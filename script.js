d376715f0f0ede204a0dbe9472c91108a0843a12
farihaorna23 <farihaorna@gmail.com>
2022-04-25 22:49:21 -0400

    finished challenge and find method

--- script.js
+++ script.js
@@ -379,3 +379,43 @@ GOOD LUCK 😀
 //   .reduce((acc, cur) => acc + cur, 0);
 
 // console.log(totalDespositInUSD); //5522.000000000001
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+*/
+
+// const calcAverageHumanAge = ages => {
+//   const averageHumanAge = ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age > 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+//   return averageHumanAge;
+// };
+
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+
+const firstWithdrawl = movements.find(mov => mov < 0);
+console.log(movements); //[200, 450, -400, 3000, -650, -130, 70, 1300]
+console.log(firstWithdrawl); //-400
+
+console.log(accounts); //the array of objects [ {account1}, {account2}]
+
+const account = accounts.find(acc => acc.owner === "Jessica Davis");
+console.log(account); //we get the entire obj //{owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222, username: 'jd'}
